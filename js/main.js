9fb627e9ee6e125af3b88de1d4073306342ee605
Optimistic <optimistic@OptimisticnoMacBook-Pro.local>
2020-12-14 04:06:24 +0900

    実装方法、id付与、ス

--- js/main.js
+++ js/main.js
@@ -2,29 +2,45 @@
 
 {
   const todos = [];
+  const tbody = document.getElementById('tbody');
+
 
   function displayTodo(todos) {
-    const tbody = document.getElementById('tbody');
-    const tr = document.createElement('tr');
-    const td1 = document.createElement('td');  
-    const td2 = document.createElement('td');
-    const stateButton = document.createElement('button');
-    const delButton = document.createElement('button');
-    delButton.textContent = '削除';
+    
+    while (tbody.firstChild){
+      tbody.removeChild(tbody.firstChild);
+    }
 
     todos.forEach((todo,i) => {
+      const tr = document.createElement('tr');
+      const td1 = document.createElement('td');  
+      const td2 = document.createElement('td');
+      const td3 = document.createElement('td');
+      const td4 =  document.createElement('td');
+      const stateButton = document.createElement('button');
+      const delButton = document.createElement('button');
+
+      
       td1.textContent = i;
       td2.textContent = todo.task;
       stateButton.textContent = todo.status;
+      delButton.textContent = '削除';
       tr.appendChild(td1);
       tr.appendChild(td2);
-      tr.appendChild(stateButton);
-      tr.appendChild(delButton);
+      td3.appendChild(stateButton);
+      td4.appendChild(delButton);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
       tbody.appendChild(tr);
+
+
     });
 
-  }
 
+
+    
+  }
+  
 
   document.getElementById('btn').addEventListener('click',() => {
     const todoComment = document.getElementById('comment');
@@ -38,6 +54,8 @@
     displayTodo(todos);
 
     });
+
+
 
 
 }
