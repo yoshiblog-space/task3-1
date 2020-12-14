8b6dd849e1ce07689f2039498d535a3f516fc576
Optimistic <optimistic@OptimisticnoMacBook-Pro.local>
2020-12-14 19:08:05 +0900

    修正後

--- js/main.js
+++ js/main.js
@@ -5,32 +5,32 @@
   const tbody = document.getElementById('tbody');
 
 
-  function displayTodo(todos) {
+  let displayTodo = (todos) => {
     
     while (tbody.firstChild){
       tbody.removeChild(tbody.firstChild);
     }
 
-    todos.forEach((todo,i) => {
+    todos.forEach((todo, idx) => {
       const tr = document.createElement('tr');
-      const td1 = document.createElement('td');  
-      const td2 = document.createElement('td');
-      const td3 = document.createElement('td');
-      const td4 =  document.createElement('td');
+      const tdIndex = document.createElement('td');  
+      const tdTask = document.createElement('td');
+      const tdState = document.createElement('td');
+      const tdDelete =  document.createElement('td');
       const stateButton = document.createElement('button');
       const delButton = document.createElement('button');
 
       
-      td1.textContent = i;
-      td2.textContent = todo.task;
+      tdIndex.textContent = idx;
+      tdTask.textContent = todo.task;
       stateButton.textContent = todo.status;
       delButton.textContent = '削除';
-      tr.appendChild(td1);
-      tr.appendChild(td2);
-      td3.appendChild(stateButton);
-      td4.appendChild(delButton);
-      tr.appendChild(td3);
-      tr.appendChild(td4);
+      tr.appendChild(tdIndex);
+      tr.appendChild(tdTask);
+      tdState.appendChild(stateButton);
+      tdDelete.appendChild(delButton);
+      tr.appendChild(tdState);
+      tr.appendChild(tdDelete);
       tbody.appendChild(tr);
 
 
@@ -42,12 +42,12 @@
   }
   
 
-  document.getElementById('btn').addEventListener('click',() => {
+  document.getElementById('btn').addEventListener('click', () => {
     const todoComment = document.getElementById('comment');
     const todo = {
-                  task: todoComment.value,
-                  status: '作業中'
-                };
+      task: todoComment.value,
+      status: '作業中'
+    };
   
     todos.push(todo);
     
